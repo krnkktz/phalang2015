f75fedb547f120f0e14d1cd8e8f222e184538f8d
Marin Roche <marin@ghettoitalics.com>
2015-10-21 15:22:34 +0200

    precedence

--- syn.ml
+++ syn.ml
@@ -23,56 +23,80 @@ let rec show = function
 
 let syn t =
 
-  let var_of_op = function
-    | Lex.Times -> "*"
-    | Lex.Division  -> "/"
-    | Lex.Modulo -> "%"
-    | Lex.Plus -> "+"
-    | Lex.Minus  -> "-"
-    | Lex.Equal  -> "=="
-    | Lex.Greater  -> ">"
-    | Lex.Lesser  -> "<"
-    | Lex.Greaterorequal  -> ">="
-    | Lex.Lesserorequal  -> "<="
-    | Lex.And  -> "and"
-    | Lex.Or  -> "or"
-    | Lex.Not -> "not"
-  in
-
-  let rec tr = function
-    | Lex.If :: xs -> let ex1, xs1 = tr_super xs in (match xs1 with
-      | Lex.Then :: xs2 -> let ex2, xs3 = tr_super xs2 in (match xs3 with
-        | Lex.Else :: xs4 -> let ex3, xs5 = tr_super xs4 in
-          Cond (ex1, ex2, ex3), xs5
-        | _ -> raise @@ Error "expected else")
-      | _ -> raise @@ Error "expected then")
+  let rec subexpr = function
+    | Lex.If :: xs -> let ex1, xs1 = expr xs in (match xs1 with
+      | Lex.Then :: xs2 ->
+        let ex2, xs3 = expr xs2 in (match xs3 with
+          | Lex.Else :: xs4 -> let ex3, xs5 = expr xs4 in
+            Cond (ex1, ex2, ex3), xs5
+          | _ -> raise @@ Error "expected else")
+        | _ -> raise @@ Error "expected then")
     | Lex.Let :: Lex.Id name :: Lex.Assign :: xs ->
-      let exp, xs1 = tr_super xs in (match xs1 with
-        | Lex.In :: xs2 -> let exp1, xs3 = tr_super xs2 in
+      let exp, xs1 = expr xs in (match xs1 with
+        | Lex.In :: xs2 -> let exp1, xs3 = expr xs2 in
           Let (name, exp, exp1), xs3
         | _ -> raise @@ Error "expected in")
     | Lex.Let :: _ -> raise @@ Error "weird let"
     | Lex.Int x :: xs -> Int x, xs
     | Lex.Bool x :: xs -> Bool x, xs
     | Lex.Id x :: xs -> Var x, xs
-    | Lex.Operator Lex.Not :: xs -> let exp, xs1 = tr_super xs in
-      Application (Var "not", exp), xs1
-    | Lex.Leftpar :: xs -> let ex, xxs = tr_super xs in (match xxs with
+    | Lex.Leftpar :: xs -> let ex, xxs = expr xs in (match xxs with
       | Lex.Rightpar :: xxxs -> ex, xxxs
       | _ -> raise @@ Error "missing right parenthesis")
-    | xs -> raise @@ Error "expected an expr" and
+    | xs -> raise @@ Error "expected a subexpr" and
 
-  tr_super xs = let exp, xs1 = tr xs in match xs1 with
-    | [] -> exp, []
-    | Lex.Operator o :: xs1 -> (try let exp1, xs2 = tr xs1 in
-        Application (Application (Var (var_of_op o), exp), exp1), xs2 with
-        | Error "expected an expr" ->
-            Application (Var (var_of_op o), exp), xs1)
-    | xs2 -> try (let exp1, xs3 = tr xs2 in Application (exp, exp1), xs3) with
-      | Error "expected an expr" -> exp, xs2 in
+  s_fun xs =
+    let rec h a xs = try (match subexpr xs with
+        | e, [] -> Application (a, e), []
+        | e, xs2 -> h (Application (a, e)) xs2) with
+      | Error "expected a subexpr" -> a, xs in
 
-  match tr_super t with
+    match subexpr xs with
+      | e, [] -> e, []
+      | e, xs1 -> h e xs1 and
+
+  s_times xs = match s_fun xs with
+    | e, Lex.Operator Lex.Times :: xs -> let e1, xs1 = s_times xs in
+      Application (Application (Var "*", e), e1), xs1
+    | e, Lex.Operator Lex.Division :: xs -> let e1, xs1 = s_times xs in
+      Application (Application (Var "/", e), e1), xs1
+    | e, Lex.Operator Lex.Modulo :: xs -> let e1, xs1 = s_times xs in
+      Application (Application (Var "%", e), e1), xs1
+    | e, xs -> e, xs and
+
+  s_plus xs = match s_times xs with
+    | e, Lex.Operator Lex.Plus :: xs -> let e1, xs1 = s_plus xs in
+      Application (Application (Var "+", e), e1), xs1
+    | e, Lex.Operator Lex.Minus :: xs -> let e1, xs1 = s_plus xs in
+      Application (Application (Var "-", e), e1), xs1
+    | e, xs -> e, xs and
+
+  s_comp xs = match s_plus xs with
+    | e, Lex.Operator Lex.Equal :: xs -> let e1, xs1 = s_comp xs in
+      Application (Application (Var "==", e), e1), xs1
+    | e, Lex.Operator Lex.Greater :: xs -> let e1, xs1 = s_comp xs in
+      Application (Application (Var ">", e), e1), xs1
+    | e, Lex.Operator Lex.Lesser :: xs -> let e1, xs1 = s_comp xs in
+      Application (Application (Var "<", e), e1), xs1
+    | e, Lex.Operator Lex.Greaterorequal :: xs -> let e1, xs1 = s_comp xs in
+      Application (Application (Var ">=", e), e1), xs1
+    | e, Lex.Operator Lex.Lesserorequal :: xs -> let e1, xs1 = s_comp xs in
+      Application (Application (Var "<=", e), e1), xs1
+    | e, xs -> e, xs and
+
+  s_bool xs = match xs with
+    | Lex.Operator Lex.Not :: xs -> let e, xs1 = s_bool xs in
+      Application (Var "not", e), xs1
+    | xs -> (match s_comp xs with
+      | e, Lex.Operator Lex.And :: xs1 -> let e1, xs2 = s_bool xs in
+        Application (Application (Var "and", e), e1), xs2
+      | e, Lex.Operator Lex.Or :: xs1 -> let e1, xs2 = s_bool xs in
+        Application (Application (Var "or", e), e1), xs2
+      | e, xs -> e, xs) and
+
+  expr xs = s_bool xs in
+
+  match expr t with
     | e, [] -> e
-    | e, xs -> raise @@Error "weird stuff in the end"
-
+    | e, xs -> raise @@ Error "how is that even possible"
 
