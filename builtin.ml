87fdd71a42600974eca830f38f62f0681f115ade
Marin Roche <marin@ghettoitalics.com>
2015-10-25 16:14:13 +0100

    recursiongit add builtin.ml !

--- builtin.ml
+++ builtin.ml
@@ -30,33 +30,39 @@ let builtin =
     "<=", c ( <= );
     "and", b ( && );
     "or", b ( || );
+    "nil", List Nil;
+
     "null", Fun ("x", Builtin
       (fun x -> Bool (x = List Nil)));
+
     "head", Fun ("x", Builtin
       (fun x -> match x with
         | List Nil -> raise @@ Error "empty list"
         | List Li (x, _) -> x
         | _ -> raise @@ Error "not a list"));
+
     "tail", Fun ("x", Builtin
       (fun x -> match x with
         | List Nil -> raise @@ Error "empty list"
         | List Li (_, xs) -> List xs
         | _ -> raise @@ Error "not a list"));
-    "nil", List Nil;
+
+
     ":", Fun ("x", Builtin
       (fun x -> Fun ("y", Builtin
         (fun y -> match y with
           | List z -> List (Li (x, z))
           | _ -> raise @@ Error "not a list"))));
-    "not", Fun ("x", Builtin (fun x -> Bool (not @@ b_ x)));
-    (* fun f ->
-      * (fun x -> f (x x))
-      * (fun x -> f (x x))
-      *)
-    "__y_combinator__", Fun ("_g_",
-      Application (
-        Fun ("_x_", Application (Var "_g_", Application (Var "_x_", Var "_x_"))),
-        Fun ("_x_", Application (Var "_g_", Application (Var "_x_", Var "_x_")))));
 
+    "not", Fun ("x", Builtin (fun x -> Bool (not @@ b_ x)));
+
+    (* fun f g  -> (fun x a -> f (x x) a) (fun x a -> f (x x) a) g *)
+    let sub = (Fun ("_x_", Fun ("_a_",
+      Application (
+        Application (Var "_f_",
+          Application (Var "_x_", Var "_x_")), Var "_a_")))) in
+
+    "__y_combinator__", Fun ("_f_", Fun ("_g_",
+      Application (Application (sub, sub), Var "_g_"))) ;
   ]
 
